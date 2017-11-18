b5c8eab65e79e797bbb5d830cc55f6bfe35b67e0
Aedan <aedan.smith@gmail.com>
2017-11-17 18:30:28 -0800

    Remove commented code

--- compiler/src/main/kotlin/io/quartz/tree/ir/DeclI.kt
+++ compiler/src/main/kotlin/io/quartz/tree/ir/DeclI.kt
@@ -37,33 +37,4 @@ sealed class DeclI : Locatable {
             val instance: TypeI,
             val impls: List<Value>
     ) : DeclI()
-
-//    data class Class(
-//            override val location: Location,
-//            val name: Name,
-//            val p: Package,
-//            val constructor: Constructor?,
-//            val constraints: List<ConstraintI>,
-//            val superTypes: List<TypeI>,
-//            val decls: List<DeclI>
-//    ) : DeclI() {
-//        data class Constructor(
-//                val args: List<TypeI>,
-//                val expr: ExprI
-//        )
-//    }
-//
-//    data class Method(
-//            override val location: Location,
-//            val name: Name,
-//            val p: Package,
-//            val scheme: Scheme,
-//            val expr: ExprI?
-//    ) : DeclI() {
-//        data class Scheme(
-//                val constraints: List<ConstraintI>,
-//                val args: List<TypeI>,
-//                val ret: TypeI
-//        )
-//    }
 }
