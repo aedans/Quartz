eb5de86a8c27083446e8705b7b8d9042dd1f52ed
Aedan <aedan.smith@gmail.com>
2017-12-05 15:50:23 -0800

    Fix function generic generation

--- compiler/src/main/kotlin/io/quartz/gen/jvm/analyze/JvmSchemeAnalyzer.kt
+++ compiler/src/main/kotlin/io/quartz/gen/jvm/analyze/JvmSchemeAnalyzer.kt
@@ -1,6 +1,6 @@
 package io.quartz.gen.jvm.analyze
 
-import io.quartz.gen.jvm.tree.*
+import io.quartz.gen.jvm.tree.JvmScheme
 import io.quartz.tree.ir.SchemeI
 
-fun SchemeI.jvm() = JvmScheme(constraints.map { JvmGeneric(it.name, it.constraint.jvm()) }, type.jvm())
+fun SchemeI.jvm() = JvmScheme(constraints.map { it.jvm() }, type.jvm())
