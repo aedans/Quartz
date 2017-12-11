f1de534b9ec57573c8ccd803a88cc3f0d28635bd
Aedan <aedan.smith@gmail.com>
2017-12-10 23:16:20 -0800

    Fix closures

--- compiler/src/main/kotlin/io/quartz/analyze/Closures.kt
+++ compiler/src/main/kotlin/io/quartz/analyze/Closures.kt
@@ -1,11 +1,12 @@
 package io.quartz.analyze
 
+import io.quartz.nil
 import io.quartz.tree.ast.ExprT
 import io.quartz.tree.util.*
 
 val ExprT.freeVariables: Set<Name> get() = when (this) {
     is ExprT.Cast -> expr.freeVariables
-    is ExprT.Var -> setOf(name.unqualified)
+    is ExprT.Var -> if (name.qualifier == nil) setOf(name.unqualified) else emptySet()
     is ExprT.Apply -> expr1.freeVariables + expr2.freeVariables
     is ExprT.If -> condition.freeVariables + expr1.freeVariables + expr2.freeVariables
     is ExprT.Lambda -> expr.freeVariables - argName
